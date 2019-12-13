c1d6e566be6ab5cdb57797019d33b3194633b19a
Sean <shiqidai1002@gmail.com>
2019-12-12 21:15:44 -0500

    update

---
+++ src/CharPredictor.py
@@ -0,0 +1,102 @@
+import argparse
+import configparser
+import os
+
+import boto3
+import numpy as np
+import cv2 as cv
+import CPR_utils as util
+import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
+
+
+def get_digits(path):
+    return pickle.load(open(path, "rb"))
+
+def get_onehot_encoder():
+    # one hot encoding
+    alphabets = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                 'J',
+                 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    classes = []
+    for a in alphabets:
+        classes.append([a])
+    ohe = OneHotEncoder(handle_unknown='ignore', categorical_features=None)
+    ohe.fit(classes)
+
+    return ohe
+
+def predict_one(digits, ohe, model, precision_threshold = 0):
+    prediction = {}
+    preds = []
+    confidence =[]
+
+    for d in digits:
+        d = np.reshape(d, (1, 28, 28, 1))
+        out = model.predict(d)
+        # Get max pre arg
+        p = []
+        precision = 0
+        for i in range(len(out)):
+            z = np.zeros(36)
+            z[np.argmax(out[i])] = 1.
+            precision = max(out[i])
+            p.append(z)
+        prediction = np.array(p)
+        pred = ohe.inverse_transform(prediction)
+
+        if precision > precision_threshold:
+            preds.append(pred[0][0])
+            confidence.append(precision)
+            print('Prediction : ' + str(pred[0][0]) + ' , Precision : ' + str(precision))
+
+    prediction['preds'] = preds
+    prediction['confidence'] = confidence
+    return prediction
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Please provide amazon credentials')
+    parser.add_argument('--conf', default="../conf/config.cfg", help='the path of config.cfg')
+    args = parser.parse_args()
+
+    config = configparser.ConfigParser()
+    config.read(args.conf)
+
+    aws_access_key_id = config['AWS_access_credentials']['aws_access_key_id']
+    aws_secret_access_key = config['AWS_access_credentials']['aws_secret_access_key']
+
+    client = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+    # Load model
+    model_key = "cnn_classifier.h5"
+    local_path = "../conf/"
+
+    model_local_path = os.path.join(local_path, model_key)
+
+    #download from s3 and update local model
+    client.download_file(config['buckets']['models'], model_key, model_local_path)
+    model = load_model(model_local_path)
+
+    digit_urls = util.get_objecturls_from_bucket(client, config['buckets']['digits'])
+
+    # predict
+    ohe = get_onehot_encoder()
+
+    local_tmp = '../predictions_tmp'
+    for url in digit_urls:
+        digits = get_digits(url)
+        prediction = predict_one(digits, ohe, model)
+
+        fname = url.split(".com/")[1].split('.')[0]
+        with open(os.path.join(local_tmp, fname), 'wb') as pf:
+            pickle.dump(prediction, pf)
+
+    # upload to s3
+    files_to_upload = os.listdir(local_tmp)
+    for f in files_to_upload:
+        util.upload_file_to_bucket(client, config['buckets']['cnnpredictions'])
+
